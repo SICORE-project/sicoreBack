f710dad95da3e49b699a65b93ab87e133d409f77
olalesam <olalesam87@gmail.com>
2026-08-21 00:20:02 +0000

    fix bug

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,31 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Schema;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    /**
+     * S'exécute automatiquement avant chaque test
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // 1. Désactive les contraintes MySQL pour permettre à RefreshDatabase de vider 'users'
+        Schema::disableForeignKeyConstraints();
+    }
+
+    /**
+     * S'exécute automatiquement après chaque test
+     */
+    protected function tearDown(): void
+    {
+        // 2. Réactive proprement les contraintes pour ne pas fausser vos requêtes d'application
+        Schema::enableForeignKeyConstraints();
+
+        parent::tearDown();
+    }
 }
